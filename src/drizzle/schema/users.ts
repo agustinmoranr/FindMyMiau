bc15a43e5816a6b39a0189810ff8a04c5f472859
Agustin Moran <agustin.moranr@gmail.com>
2025-03-09 12:58:07 -0600

    añade campo de número telefónico en tabla users.

--- src/drizzle/schema/users.ts
+++ src/drizzle/schema/users.ts
@@ -5,6 +5,7 @@ export const userTable = pgTable('users_table', {
 	id: id,
 	name: text('name').notNull(),
 	email: text('email').notNull().unique(),
+	phone_number: text('phone_number').notNull().unique(),
 	password: text('password').notNull(),
 	created_at: createdAt,
 	updated_at: updatedAt,

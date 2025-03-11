dfbecb21494d8cdc367aa3aab174026566542d9b
Agustin Moran <agustin.moranr@gmail.com>
2025-03-11 16:41:04 -0600

    configura correctamente la redirección al hacer sign in con un provider, para los ambientes de desarrollo y producción

--- src/env.d.ts
+++ src/env.d.ts
@@ -2,6 +2,7 @@ interface ImportMetaEnv {
 	readonly DATABASE_URL: string;
 	readonly PUBLIC_SUPABASE_URL: string;
 	readonly PUBLIC_SUPABASE_ANON_KEY: string;
+	readonly PUBLIC_DOMAIN_URL: string;
 	// more env variables...
 }
 

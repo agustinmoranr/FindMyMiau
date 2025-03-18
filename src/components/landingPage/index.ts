c096889fcb3287d83256e19aba8ec69017f81637
Agustin Moran <agustin.moranr@gmail.com>
2025-03-18 10:19:29 -0600

    refactor: estructura de componentes en proyecto

---
+++ src/components/landingPage/index.ts
@@ -0,0 +1,17 @@
+export * from './Hero.astro';
+export { default as Hero } from './Hero.astro';
+
+export * from './HowItWorks.astro';
+export { default as HowItWorks } from './HowItWorks.astro';
+
+export * from './FeaturesSection.astro';
+export { default as FeaturesSection } from './FeaturesSection.astro';
+
+export * from './Testimonials.astro';
+export { default as Testimonials } from './Testimonials.astro';
+
+export * from './FAQSection.astro';
+export { default as FAQSection } from './FAQSection.astro';
+
+export * from './CTASection.astro';
+export { default as CTASection } from './CTASection.astro';
